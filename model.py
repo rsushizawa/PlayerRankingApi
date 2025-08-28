e5bd664ccbd0cb5c97cb89e7211587bec593ea3e
rsushizawa <rsushizawa@gmail.com>
2025-08-28 16:54:31 -0300

    fix update default

--- model.py
+++ model.py
@@ -4,12 +4,12 @@ from typing import Optional
 class Player(BaseModel):
     id: int
     name: str
-    points: int = Field(gt=0, detail="Field must be positive")
+    points: int = Field(gt=0)
 
 class CreatePlayer(BaseModel):
     name: str
-    points: int = Field(gt=0, detail="Field must be positive") 
+    points: int = Field(gt=0) 
 
 class UpdatePlayer(BaseModel):
     name: Optional[str] = None
-    points: Optional[int] = Field(gt=0, detail="Field must be positive")
+    points: Optional[int] = Field(default=None, gt=0)
